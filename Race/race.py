d1b8fc495f56796caa61a725a65242f1bfabe884
CKassab <40821313+CKassab@users.noreply.github.com>
2022-03-20 14:27:55 +0000

    Add files via upload

---
+++ Race/race.py
@@ -0,0 +1,22 @@
+## script that starts the race
+
+
+
+from pixyBot import pixyBot
+from pixyCam import pixyCam
+from controlwithobstacles import robotRace
+from PIDcontroller import PID_controller
+
+# exercises
+def main():
+    ### IMPORTANT
+    servoCorrection = 0 # put the servo correction for your robot here
+    ###
+
+    r = pixyBot(servoCorrection, PID_controller(0.08, 0, 0.1))
+    p = pixyCam()
+    rR = robotRace(r, p)
+
+    rR.race(0.5)
+    #rR.test2angles()
+main()
